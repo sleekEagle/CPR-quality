9b99262aa4320b3e8af527ad19ca94f5a99df0ac
sleekEagle <lahirunuwanisme@gmail.com>
2024-02-15 14:21:00 -0500

    cpr prediction updates

--- CPR_prediction.py
+++ CPR_prediction.py
@@ -4,9 +4,10 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-session_dir=r'D:\CPR_data_raw\P0\extracted\s_0'
+session_dir=r'D:\CPR_data_raw\P0\extracted\s_1'
 kinect_ts_path=os.path.join(session_dir,'kinect_ts.txt')
 XYZ_dict_path=os.path.join(session_dir,'hand_keypts_XYZ.json')
+kinect_depth_path=os.path.join(session_dir,'kinect_depth_interp.txt')
 
 with open(XYZ_dict_path, 'r') as file:
     XYZ_dict = json.load(file)
@@ -14,6 +15,8 @@ with open(XYZ_dict_path, 'r') as file:
 kinect_ts_list=utils.read_allnum_lines(kinect_ts_path)
 sorted_keys=[key for key in XYZ_dict.keys()]
 sorted_keys.sort()
+
+kinect_inter_depth_list=utils.read_allnum_lines(kinect_depth_path)
 
 XYZ_list=[]
 for key in sorted_keys:
@@ -31,6 +34,37 @@ t=t/max(t)
 pred=utils.interpolate_between_ts(XYS_sqsum_norm,t,t,fit_window=50,deg=4)
 
 peaks,valleys=utils.find_peaks_and_valleys(pred)
+len_peaks=len(peaks)
+len_valleys=len(valleys)
+l=min(len_peaks,len_valleys)
+peaks=peaks[:l]
+valleys=valleys[:l]
+
+fit_window=50
+deg=4
+x_pred=utils.interpolate_between_ts(XYZ_array[:,0],t,t,fit_window=fit_window,deg=deg)
+y_pred=utils.interpolate_between_ts(XYZ_array[:,1],t,t,fit_window=fit_window,deg=deg)
+z_pred=utils.interpolate_between_ts(XYZ_array[:,2],t,t,fit_window=fit_window,deg=deg)
+
+XYZ_val_pred=np.array([x_pred,y_pred,z_pred])
+high_vals=XYZ_val_pred[:,peaks]
+low_vals=XYZ_val_pred[:,valleys]
+depths=np.sum((high_vals-low_vals)**2,axis=0)**0.5
+
+#get GT depths
+kinect_inter_depth_array=np.array(kinect_inter_depth_list)
+GT_high_vals=kinect_inter_depth_array[peaks]
+GT_low_vals=kinect_inter_depth_array[valleys]
+GT_depths=(GT_high_vals-GT_low_vals)
+
+
+depth_sorted_indices = np.argsort(depths)
+
+plt.plot((depths*2))
+plt.plot(GT_depths)
+plt.show()
+
+np.mean(np.abs((depths*2)-GT_depths))
 
 
 
