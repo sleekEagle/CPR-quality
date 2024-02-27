ab22af6a7c824b8a27b82a24622177e6b9948249
sleekEagle <lahirunuwanisme@gmail.com>
2024-02-27 10:46:03 -0500

    use args inhandseg

--- data_prep/handseg.py
+++ data_prep/handseg.py
@@ -11,52 +11,75 @@ from segment_anything import SamPredictor
 import numpy as np
 from functools import reduce
 from scipy.ndimage.morphology import binary_fill_holes
+import sys
 
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-MODEL_TYPE = "vit_h"
+def main():
+    # model_path e.g: C:\Users\lahir\code\CPR-quality\sam_vit_h_4b8939.pth
+    # data_path e.g : D:\CPR_data_raw
+    model_path = sys.argv[1]
+    data_path = sys.argv[2]
 
-CHECKPOINT_PATH=r'C:\Users\lahir\code\CPR-quality\sam_vit_h_4b8939.pth'
-sam = sam_model_registry[MODEL_TYPE](checkpoint=CHECKPOINT_PATH)
-sam.to(device=DEVICE)
-mask_predictor = SamPredictor(sam)
+    DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    MODEL_TYPE = "vit_h"
 
-root_dir='D:\\CPR_data_raw'
-subj_dirs=[os.path.join(root_dir,item,'extracted') for item in utils.list_subdirectories(root_dir) if item[0].lower()=='p']
-for subj_dir in subj_dirs:
-    session_dirs=[os.path.join(subj_dir,session_dir) for session_dir in utils.list_subdirectories(subj_dir) if session_dir[0].lower()=='s']
-    for session_dir in session_dirs:
-        print(session_dir)
-        hand_bb_path=os.path.join(session_dir,'hand_bbs.json')
-        if not os.path.exists(hand_bb_path):
-            print(f'{hand_bb_path} does not exist')
-            continue
-        with open(hand_bb_path, 'r') as file:
-            data = json.load(file)
-        hand_mask_dir=os.path.join(session_dir,'hand_mask')
-        os.makedirs(hand_mask_dir,exist_ok=True)
+    CHECKPOINT_PATH=model_path
+    sam = sam_model_registry[MODEL_TYPE](checkpoint=CHECKPOINT_PATH)
+    sam.to(device=DEVICE)
+    mask_predictor = SamPredictor(sam)
 
-        img_files=utils.list_files(os.path.join(session_dir,'color'),'jpg')
-        for img_file in img_files:
-            hand_mask_img_path=os.path.join(hand_mask_dir,os.path.splitext(img_file)[0]+'.png')
-            if os.path.exists(hand_mask_img_path):
+    root_dir=data_path
+    subj_dirs=[os.path.join(root_dir,item,'extracted') for item in utils.list_subdirectories(root_dir) if item[0].lower()=='p']
+    for subj_dir in subj_dirs:
+        session_dirs=[os.path.join(subj_dir,session_dir) for session_dir in utils.list_subdirectories(subj_dir) if session_dir[0].lower()=='s']
+        for session_dir in session_dirs:
+            print(session_dir)
+            hand_bb_path=os.path.join(session_dir,'hand_bbs.json')
+            if not os.path.exists(hand_bb_path):
+                print(f'{hand_bb_path} does not exist')
                 continue
+            with open(hand_bb_path, 'r') as file:
+                data = json.load(file)
+            hand_mask_dir=os.path.join(session_dir,'hand_mask')
+            os.makedirs(hand_mask_dir,exist_ok=True)
 
-            bbx_str=data[img_file.split('.')[0]]
-            bbx=[int(item) for item in bbx_str.split(',')]
+            img_files=utils.list_files(os.path.join(session_dir,'color'),'jpg')
+            for img_file in img_files:
+                hand_mask_img_path=os.path.join(hand_mask_dir,os.path.splitext(img_file)[0]+'.png')
+                if os.path.exists(hand_mask_img_path):
+                    continue
 
-            image_path = os.path.join(session_dir, 'color', img_file)
-            image = cv2.imread(image_path)
-            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            mask_predictor.set_image(image_rgb)
-            masks, scores, logits = mask_predictor.predict(
-            box=np.array(bbx),
-            multimask_output=True
-            )
+                bbx_str=data[img_file.split('.')[0]]
+                bbx=[int(item) for item in bbx_str.split(',')]
 
-            mask=np.any(masks, axis=0)*255
-            mask=mask.astype(np.uint8)
-            cv2.imwrite(hand_mask_img_path, mask)            
+                image_path = os.path.join(session_dir, 'color', img_file)
+                image = cv2.imread(image_path)
+                image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                mask_predictor.set_image(image_rgb)
+                masks, scores, logits = mask_predictor.predict(
+                box=np.array(bbx),
+                multimask_output=True
+                )
 
+                mask=np.any(masks, axis=0)*255
+                mask=mask.astype(np.uint8)
+                cv2.imwrite(hand_mask_img_path, mask)            
+
+
+if __name__ == "__main__":
+    main()
+
+    import sys
+
+    def main():
+        # Access command line arguments
+        arg1 = sys.argv[1]  # First argument
+        arg2 = sys.argv[2]  # Second argument
+        # Rest of your code...
+
+    if __name__ == "__main__":
+        main()
+
+        
 
 
 
